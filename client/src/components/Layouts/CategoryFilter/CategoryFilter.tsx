62f33467043d553bdc8ea7da6246310684aea93c
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2026-05-20 13:25:50 +0200

    client: created category filter

---
+++ client/src/components/Layouts/CategoryFilter/CategoryFilter.tsx
@@ -0,0 +1,32 @@
+import type { Category } from '@/types/common';
+
+import CategoryButton from '@/components/Buttons/CategoryButton/CategoryButton';
+import { StarIcon } from 'lucide-react';
+
+interface CategoryFilterProps {
+	categories: Category[];
+	activeCategory: string | null;
+	onCategoryChange: (_categoryId: string | null) => void;
+}
+
+const CategoryFilter = ({ categories, activeCategory, onCategoryChange }: CategoryFilterProps) => {
+	const mainCategory: Category = {
+		id: null,
+		label: 'All Categories',
+		icon: StarIcon
+	};
+
+	return (
+		<div className='flex flex-wrap gap-3'>
+			<CategoryButton category={mainCategory} activeCategory={activeCategory} onClick={() => onCategoryChange(null)} />
+
+			{categories.map(category => {
+				return (
+					<CategoryButton key={category.id} category={category} activeCategory={activeCategory} onClick={onCategoryChange} />
+				);
+			})}
+		</div>
+	);
+};
+
+export default CategoryFilter;
