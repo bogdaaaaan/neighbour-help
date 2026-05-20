a14d8f660a13458fa9985f8ec65e7ef0a28f65c2
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2026-05-20 14:07:41 +0200

    client: added footer and fixed some styling

--- client/src/components/Layouts/Navbar/Navbar.tsx
+++ client/src/components/Layouts/Navbar/Navbar.tsx
@@ -3,35 +3,37 @@ import { UserCircleIcon } from 'lucide-react';
 
 const Navbar = () => {
 	return (
-		<nav className='bg-white border-b border-slate-200'>
-			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-				<div className='flex items-center justify-between h-16'>
-					<div className='flex items-center gap-8'>
-						<Link to='/' className='text-xl text-slate-900 hover:text-teal-600 transition-colors'>
+		<header className='sticky top-0 z-50'>
+			<nav className='bg-white border-b border-slate-200'>
+				<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+					<div className='flex items-center justify-between h-16'>
+						<div className='flex items-center gap-8'>
+							<Link to='/' className='text-xl text-slate-900 hover:text-teal-600 transition-colors'>
 							NeighbourHelp
-						</Link>
-
-						<div className='hidden md:flex items-center gap-6'>
-							<Link to='/dashboard' className='text-slate-600 hover:text-slate-900 transition-colors'>
-								Browse Board
 							</Link>
+
+							<div className='hidden md:flex items-center gap-6'>
+								<Link to='/dashboard' className='text-slate-600 hover:text-slate-900 transition-colors'>
+								Browse Board
+								</Link>
+							</div>
+						</div>
+
+						<div className='flex items-center gap-4'>
+							<Link to='/create' className='px-4 py-2 bg-teal-600 text-white rounded-lg hover:bg-teal-700 transition-colors'>
+							Create Request
+							</Link>
+
+							<button className='flex items-center gap-2 text-slate-700 hover:text-slate-900 transition-colors'>
+								<Link to='/profile' className='flex items-center justify-center'>
+									<UserCircleIcon className='w-8 h-8 rounded-full text-teal-600 ' />
+								</Link>
+							</button>
 						</div>
 					</div>
-
-					<div className='flex items-center gap-4'>
-						<Link to='/create' className='px-4 py-2 bg-teal-600 text-white rounded-lg hover:bg-teal-700 transition-colors'>
-							Create Request
-						</Link>
-
-						<button className='flex items-center gap-2 text-slate-700 hover:text-slate-900 transition-colors'>
-							<Link to='/profile' className='flex items-center justify-center'>
-								<UserCircleIcon className='w-8 h-8 rounded-full text-teal-600 ' />
-							</Link>
-						</button>
-					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
+		</header>
 	);
 };
 
--- client/src/pages/Root/Root.tsx
+++ client/src/pages/Root/Root.tsx
@@ -1,22 +1,19 @@
 import { Outlet } from 'react-router';
 
 import Navbar from '@/components/Layouts/Navbar/Navbar';
+import Footer from '@/components/Layouts/Footer/Footer';
 
 const Root = () => {
 	return (
-		<>
-			<header className='sticky top-0 z-50'>
-				<Navbar/>
-			</header>
+		<div className='min-h-screen flex flex-col'>
+			<Navbar/>
 
-			<main>
+			<main className='flex-1'>
 				<Outlet />
 			</main>
 
-			<footer>
-
-			</footer>
-		</>
+			<Footer/>
+		</div>
 
 	);
 };
