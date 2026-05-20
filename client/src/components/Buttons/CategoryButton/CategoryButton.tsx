dc5eb66869fa9a30a20fbbf8cecfa22c5e05f8cc
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2026-05-20 13:25:31 +0200

    client: create category button component

---
+++ client/src/components/Buttons/CategoryButton/CategoryButton.tsx
@@ -0,0 +1,30 @@
+import type { Category } from '@/types/common';
+
+interface CategoryButtonProps {
+	category: Category;
+	activeCategory: string | null;
+	onClick: (_categoryId: string | null) => void;
+}
+
+const CategoryButton = ({ category, activeCategory, onClick }: CategoryButtonProps) => {
+	const Icon = category.icon;
+
+	return (
+		<>
+			<button
+				onClick={() => onClick(category.id)}
+				className={`px-4 py-2 rounded-lg transition-all flex items-center gap-2 cursor-pointer border ${
+					activeCategory === category.id
+						? 'bg-teal-600 text-white shadow-sm'
+						: 'bg-white text-slate-600 '
+				}`}
+			>
+				<Icon className='w-4 h-4' />
+				<span>{category.label}</span>
+			</button>
+		</>
+
+	);
+};
+
+export default CategoryButton;
