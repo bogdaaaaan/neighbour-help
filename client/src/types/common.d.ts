a45ef4cf86c9f511c85c64bbd0dd2a1156a64139
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2026-05-20 13:23:43 +0200

    client: added common types

---
+++ client/src/types/common.d.ts
@@ -0,0 +1,16 @@
+import type { LucideIcon } from 'lucide-react';
+
+export interface Category {
+	id: string | null;
+	label: string;
+	icon: LucideIcon;
+}
+
+export interface HelpRequest {
+	id: number;
+	title: string;
+	category: string;
+	description: string;
+	author: string;
+	date: string;
+}
